5ee68357baa468433874d66aecab2a92658f62c2
Dave Bry <dave@brytech.dev>
2026-01-03 20:56:38 -0500

    improvements to showCard component

--- src/app/ui/showCard/showCard.tsx
+++ src/app/ui/showCard/showCard.tsx
@@ -1,33 +1,30 @@
+'use client';
+
 import { useState } from 'react';
 import { isShowInLibrary, addRemoveShow } from '@/lib/localStorageService';
 import { Show } from '@/lib/definitions';
 import styles from './showCard.module.css';
 
 export default function ShowCard({ show }: { show: Show }) {
-  const [isInLibrary, setIsInLibrary] = useState(isShowInLibrary(show.id));
+  const [isInLibrary, setIsInLibrary] = useState<boolean>(() => isShowInLibrary(show.id));
 
   const tmdbImgUrlBase = 'https://image.tmdb.org/t/p/w780';
   const backgroundStyle =
-    show.backdrop_path ?
-        { backgroundImage: `url(${`${tmdbImgUrlBase}${show.backdrop_path}`})` }
-        : {};
+    show.backdrop_path ? { backgroundImage: `url(${tmdbImgUrlBase}${show.backdrop_path})` } : {};
 
-  const linkClass =
-    isInLibrary ? styles.linkRemove : styles.linkAdd;
-
-  const linkText =
-    isInLibrary ? "Remove" : "Add";
+  const toggleText = isInLibrary ? 'Remove' : 'Add';
+  const toggleTextClass = isInLibrary ? styles.toggleTextRemove : styles.toggleTextAdd;
 
   const handleClick = () => {
     addRemoveShow(show);
-    setIsInLibrary((prev: boolean) => !prev);
+    setIsInLibrary((prev) => !prev);
   };
 
   return (
-    <div className={styles.card} style={backgroundStyle} onClick={handleClick}>
-      <span className={styles.showName}>{ show.name }</span>
-      <div className={styles.cardLinks}>
-        <a className={linkClass}>{ linkText }</a>
+    <div className={styles.card} style={backgroundStyle} onClick={handleClick} role="button">
+      <span className={styles.showName}>{show.name}</span>
+      <div className={`${styles.cardToggle} ${toggleTextClass}`}>
+        {toggleText}
       </div>
     </div>
   );
