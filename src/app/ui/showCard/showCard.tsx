e804d05f25d21ca6e61aa051f2ae399818f83377
Dave Bry <dave@brytech.dev>
2026-01-30 17:37:21 -0500

    use shadcn Card component for showCard

--- src/app/ui/showCard/showCard.tsx
+++ src/app/ui/showCard/showCard.tsx
@@ -1,8 +1,14 @@
 'use client';
 import { useState } from 'react';
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 import { isShowInLibrary, addRemoveShow } from '@/lib/localStorageService';
 import { Show } from '@/lib/definitions';
-import styles from './showCard.module.css';
 
 export default function ShowCard({ show }: { show: Show }) {
   const [isInLibrary, setIsInLibrary] = useState<boolean>(() =>
@@ -16,8 +22,8 @@ export default function ShowCard({ show }: { show: Show }) {
 
   const toggleText = isInLibrary ? 'Remove' : 'Add';
   const toggleTextClass = isInLibrary
-    ? styles.toggleTextRemove
-    : styles.toggleTextAdd;
+    ? 'text-red-remove'
+    : 'text-green-add';
 
   const handleClick = () => {
     addRemoveShow(show);
@@ -25,16 +31,20 @@ export default function ShowCard({ show }: { show: Show }) {
   };
 
   return (
-    <div
-      className={styles.card}
-      style={backgroundStyle}
+    <Card
+      className="relative mx-auto w-full max-w-sm pt-0 gap-2 pb-0 border-2 border-gray-500 rounded-md hover:border-primary hover:border-4 hover:scale-104 transition-all duration-200 ease-linear cursor-pointer bg-center bg-cover bg-no-repeat aspect-video"
       onClick={handleClick}
       role="button"
+      style={backgroundStyle}
     >
-      <span className={styles.showName}>{show.name}</span>
-      <div className={`${styles.cardToggle} ${toggleTextClass}`}>
-        {toggleText}
-      </div>
-    </div>
+      <CardHeader>
+        <CardTitle className="relative inline-flex top-4 z-50 mx-auto px-4 py-2 max-w-85 bg-gray-light border-transparent rounded-lg text-white font-bold w-fit">{show.name}</CardTitle>
+      </CardHeader>
+      <CardFooter className="w-full px-0 py-1 h-8 absolute bottom-0">
+        <Button className={`w-full border-0 z-50 h-8 px-0 rounded-none bg-gray-dark font-bold text-base hover:bg-gray-dark ${toggleTextClass}`}>
+          {toggleText}
+        </Button>
+      </CardFooter>
+    </Card>
   );
 }
